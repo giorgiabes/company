92de08b0fcfaa987a3e49e632075a5e8c5b1d757
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2025-03-03 07:21:06 +0000

    add about page class based view

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 
 def home_page_view(request):
-    return render(request, "home.html")
+    context = {
+        "inventory_list": ["Wiget 1", "Wiget 2", "Wiget 3"],
+        "greeting": "THank you FOR visitING",
+    }
+    return render(request, "home.html", context)
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-11-22-11"
+        return context
